b22a259b9bb2df04f558dbd42162b0d11418ed0b
Patrick Khouri <patrick.darya@gmail.com>
2022-04-15 15:05:32 -0300

    Did the mints sector

--- src/Sections.js
+++ src/Sections.js
@@ -1,13 +1,16 @@
 import React from 'react';
 import Investments from './Investments'
+import Mints from './Mints'
 
 const Sections = ({todaysNews, headers}) => {
   const {investmentsUpdates, security,  mints, nftNews, others} = todaysNews
   const investmentsUpdatesHeader = headers.investmentsUpdates
+  const mintsHeaders = headers.mints
 
   return (<section>
     <h5>Sections</h5>
     <Investments investmentsUpdates={investmentsUpdates} investmentsUpdatesHeader={investmentsUpdatesHeader}/>
+    <Mints mints={mints} mintsHeaders={mintsHeaders}/>
   </section>
   )}
 
